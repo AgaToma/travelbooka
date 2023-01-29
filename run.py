97b797c31e48382457ae5f540dcd104a760ddc14
AgaToma <aga.tomashevska@gmail.com>
2023-01-29 12:45:53 +0000

    Import external modules, add logo diplay function

--- run.py
+++ run.py
@@ -1,3 +1,38 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# google drive api and google sheets imports and constants
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file("creds.json")
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open("Travelbooka")
+
+# pyfiglet import to generate logo in ASCII
+import pyfiglet
+
+# import colorama for adding colour
+import colorama
+from colorama import Fore
+colorama.init(autoreset=True)
+
+
+
+def logo():
+    """
+    App logo and description
+    """
+    logo = pyfiglet.figlet_format(
+        "Travelbooka", font="standard", justify="center")
+    print(logo)
+
+    print(Fore.RED + "Create".center(20) + Fore.MAGENTA + "your".center(20) +
+    Fore.CYAN + "own".center(20) + Fore.YELLOW + "travel".center(20) + "\n")
+
+logo() 
