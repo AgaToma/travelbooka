c7fadb37f5489ecec5434d8bb3a9c2d6c4c62a02
AgaToma <aga.tomashevska@gmail.com>
2023-02-01 22:57:28 +0000

    Add os and tabulate module, 2 more input collections

--- run.py
+++ run.py
@@ -14,6 +14,9 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("Travelbooka")
 
+# os import to clear terminal for user
+import os
+
 # pyfiglet import to generate logo in ASCII
 import pyfiglet
 
@@ -25,6 +28,8 @@ colorama.init(autoreset=True)
 # import datetime module to work with holiday date
 import datetime
 
+# import tabulate to display data in tables
+from tabulate import tabulate
 
 
 def logo_display():
@@ -43,20 +48,50 @@ def logo_display():
 
 
 
-def date_input ():
+def date_input():
     """
     Takes user desired travel date, stores it in a variable
     to be used to determine Activity holidays type, if Activity holidays
     are selected
     """
-    date_entry = input("Enter a date in YYYY-MM-DD format: ")
+    date_entry = input("Enter a date in YYYY-MM-DD format: " + "\n")
     user_month = date_entry.split('-')[1]
     
     return user_month
+
+def budget_input():
+    """
+    Takes user target budget, stores it in a variable
+    to be used during offer selection
+    """
+    budget_entry = input("Enter your target budget in number format: " + "EUR ")
+    return budget_entry
+
+def people_count():
+    """
+    Takes user information on the amount of people going
+    """
+    people_entry = input("Enter number of people on the booking: " + "\n")
+    return people_entry
+
+def display_hol_types():
+    """
+    Displays holiday types from google sheet
+    """
+    os.system('cls' if os.name == 'nt' else "printf '\033c'")
+
+    holiday_types = SHEET.worksheet("holiday_types")
+    data = holiday_types.get_all_values()
+
+    col_names = data[0]
+    print(col_names)
 
 
 
 
 logo_display() 
 date_input()
+people_count()
+budget_input()
+display_hol_types()
 
